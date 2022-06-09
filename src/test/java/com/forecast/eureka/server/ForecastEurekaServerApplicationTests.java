b2667e84b8d9268077d8aaed1828e70930a7fb25
Raju-Chigicherla <106733364+Raju-Chigicherla@users.noreply.github.com>
2022-06-09 13:48:35 +0530

    Added ForecastEurekaServerApplicationTests.java

---
+++ src/test/java/com/forecast/eureka/server/ForecastEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forecast.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
